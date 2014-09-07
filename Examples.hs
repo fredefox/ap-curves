e204cc6cdabbf2375b92c11e4cb0806924b5466a
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2014-09-07 16:27:33 +0200

    Add hilbert-function
    Implementation is just the reference implementation from the
    course webpage

--- Examples.hs
+++ Examples.hs
@@ -8,3 +8,15 @@ import Export
  - Example curves with the `Curves`-library
  -
  -}
+hilbert :: Curve -> Curve
+hilbert c = c0 `connect` c1 `connect` c2 `connect` c3
+   where  w = width c
+          h = height c
+          p = 10
+
+          ch = reflect c Horizontal 0
+
+          c0 = ch `rotate` (-90) `translate` (point (w+p+w, h+p+h))
+          c1 = c `translate` (point (w+p+w, h))
+          c2 = c
+          c3 = ch `rotate` 90 `translate` (point (0, h+p))
